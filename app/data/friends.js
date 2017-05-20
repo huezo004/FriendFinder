a922970466008c89aebb52da2df5420fe12babd4
Denyse Huezo <denyse_004@hotmail.com>
2017-05-20 03:04:10 -0400

    f

---
+++ app/data/friends.js
@@ -0,0 +1,44 @@
+
+var friends = [
+   {
+      "name":"J Balvin",
+      "photo":"https://en.wikipedia.org/wiki/J_Balvin#/media/File:J_Balvin_2015.jpg",
+      "scores":[5,1,4,4,5,1,2,5,4,1]
+   },
+   {
+      "name":"Khal Drgo",
+      "photo":"https://en.wikipedia.org/wiki/Khal_Drogo#/media/File:Khal_Drogo-Jason_Momoa.jpg",
+      "scores":[4,2,5,1,3,2,2,1,3,2]
+   },
+   {
+      "name":"Mr. Chopra ",
+      "photo":"https://en.wikipedia.org/wiki/Deepak_Chopra#/media/File:Deepak_Chopra.jpg",
+      "scores":[5,2,2,2,4,1,3,2,5,5]
+   },
+   {
+      "name":"Mr. Ears",
+      "photo":"https://en.wikipedia.org/wiki/French_Bulldog#/media/File:Mi_Gran_Amigo_El_General.jpg",
+      "scores":[3,3,4,2,2,1,3,2,2,3]
+   },
+   {
+      "name":"Edward Snowden",
+      "photo":"https://upload.wikimedia.org/wikipedia/commons/6/60/Edward_Snowden-2.jpg",
+      "scores":[4,3,4,1,5,2,5,3,1,4]
+   },
+      {
+      "name":"HH Nader",
+      "photo":"https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/Aatef_Jenyat.jpg/293px-Aatef_Jenyat.jpg",
+      "scores":[3,3,4,1,4,3,5,3,5,4]
+   },
+   {
+      "name":"TT Swift",
+      "photo":"http://www.billboard.com/files/styles/article_main_image/public/stylus/1235529-taylor-swift-woman-of-the-year-617-409.jpg",
+      "scores":[4,4,2,3,2,2,3,2,4,5]
+   }
+
+]
+
+
+module.exports = friends;
+
+
